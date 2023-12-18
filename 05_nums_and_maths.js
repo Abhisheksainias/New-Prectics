612a05c715a308887aaee93d4187dd9338bd5063
Abhisheksainias <abhisheksaini4952@gmail.com>
2023-12-18 23:37:28 +0530

    math

---
+++ 05_nums_and_maths.js
@@ -0,0 +1,37 @@
+const  score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumbar =1234.66252
+
+// console.log(otherNumbar.toPrecision(4));
+
+const hun = 1000000
+// console.log(hun.toLocaleString('en-IN'));
+
+
+// *********** MATHS ************
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6))
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(4,5,3,8,2));
+// console.log(Math.max(4,5,3,8,2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
