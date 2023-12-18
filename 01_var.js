a5f91583f52c5aa0c9af570439af01eefae2bb73
Abhisheksainias <abhisheksaini4952@gmail.com>
2023-12-18 11:13:30 +0530

    parctices

---
+++ 01_var.js
@@ -0,0 +1,15 @@
+const accountId = 124235;
+let accountEmail = "abhishek@gmail.com"
+var accountPassward = "12345"
+
+accountCity = "Alwar"
+
+// accountId = 441545;
+
+accountEmail = "absaini@gmail.com"
+
+accountPassward = "25314"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassward, accountCity])
+
---
+++ 02_conversion.js
@@ -0,0 +1,8 @@
+console.log(2 + 4);
+console.log("2" + 3);
+console.log(2 + "5");
+console.log(2 + 5 + "5");
+console.log(2 % 7);
+
+console.log(2 + 4 - (((5 * 8) / 6) % 8)); // don't use this type to code any condition
+// this id not readle this is a messy type of code
